7f027bfd80ac32e4c373bf91860e175dc0914556
Ehsan Hussain <ehsanhussain899@gmail.com>
2026-01-16 11:19:29 -0500

    Added (unplantable) garlic with a temp texture.

--- src/main/java/net/nashe/problematicvampiresmod/item/ModFoodProperties.java
+++ src/main/java/net/nashe/problematicvampiresmod/item/ModFoodProperties.java
@@ -5,5 +5,5 @@ import net.minecraft.world.food.FoodProperties;
 
 public class ModFoodProperties {
     public static final FoodProperties GARLIC = new FoodProperties.Builder().nutrition(1)
-            .saturationModifier(0.45f).build();
+            .saturationModifier(0.6f).build();
 }
